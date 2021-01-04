3d535dcc2446458c5c6e4a5b0be5edcd95dcb282
mohaOahmadi <moha.1111@gmail.com>
2021-01-04 18:01:52 +0000

    Added a new Angular Material Module for import.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  definition: Array<any> = [
+    { short: "Pt",
+     desc: "Transmitted Power" },
+     { short: "Gt",
+     desc: "Transmitter antenna gain" },
+     { short: "Gr",
+     desc: "Receiver antenna gain" },
+     { short: "Pr",
+     desc: "Received power" },
+     { short: "Өaz",
+     desc: "Horizontal antenna beamwidth" },
+     { short: "φel",
+     desc: "Vertical antenna beamwidth" },
+     { short: "Ls",
+     desc: "Space loss resulting from attenuation and spreading" },
+     { short: "MHz",
+     desc: "Frequency used by system" },
+     { short: "D",
+     desc: "Distance of the transmittion" },
+     { short: "Smin",
+     desc: "Receiver sensitivity" },
+  ];
+
   constructor() {}
+
+  ngOnInit(){
+
+  }
 
 }
