5f90ffc0c98e90f45fa16b3bf39b9cf6be0d287a
mohaOahmadi <moha.1111@gmail.com>
2021-01-15 16:51:02 +0000

    feat: converter tab
    Added a card to convert distance. i.e. km to mi and vise versa
    changed tabs icons

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,3 +1,4 @@
+import { Input } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  shouldHide: boolean = true;
+  kilometer: number;
+  mile: number;
 
   constructor() {}
+
+  ngOnInit(){}
+
+  hide(){
+    if (!this.shouldHide){
+      this.shouldHide = !this.shouldHide;
+    }
+    else
+    {
+      this.shouldHide = !this.shouldHide;
+    }
+    
+    console.log("card is hidden");
+  }
+
+  calculate(){
+      this.mile = this.kilometer * 0.621371;
+      console.log(this.mile);
+
+  }
 
 }
